f3e6b0b2cea390a41233299483319cbf36667971
Mia Borgia <70165410+am0eba-byte@users.noreply.github.com>
2021-10-22 12:43:13 -0400

    finish godRelicRefs javascript toggle

--- docs/javascript/godRefs.js
+++ docs/javascript/godRefs.js
@@ -8,22 +8,120 @@ function init() {
 function toggle() {
     var id = this.id;
     switch (id) {
-        case "CHARtoggle": {
-            var chars = document.getElementsByClassName("character");
+        case "OneHtoggle": {
+            var chars = document.getElementsByClassName("OneH");
             for (var i = 0; i < chars.length; i++) {
                 chars[i].classList.toggle("on")
             }
         };
         break;
-        case "PLtoggle": {
-            var places = document.getElementsByClassName("place");
+        case "Chatoggle": {
+            var places = document.getElementsByClassName("Cha");
             for (var i = 0; i < places.length; i++) {
                 places[i].classList.toggle("on")
             }
         };
         break;
-        case "OBtoggle": {
-            var objects = document.getElementsByClassName("object");
+        case "Xbatoggle": {
+            var objects = document.getElementsByClassName("Xba");
+            for (var i = 0; i < objects.length; i++) {
+                objects[i].classList.toggle("on")
+            }
+        };
+        break;
+        case "Huntoggle": {
+            var objects = document.getElementsByClassName("Hun");
+            for (var i = 0; i < objects.length; i++) {
+                objects[i].classList.toggle("on")
+            }
+        };
+        break;
+        case "PBDtoggle": {
+            var objects = document.getElementsByClassName("PBD");
+            for (var i = 0; i < objects.length; i++) {
+                objects[i].classList.toggle("on")
+            }
+        };
+        break;
+        case "SPStoggle": {
+            var objects = document.getElementsByClassName("SPS");
+            for (var i = 0; i < objects.length; i++) {
+                objects[i].classList.toggle("on")
+            }
+        };
+        break;
+        case "LOXtoggle": {
+            var objects = document.getElementsByClassName("LOX");
+            for (var i = 0; i < objects.length; i++) {
+                objects[i].classList.toggle("on")
+            }
+        };
+        break;
+        case "SanBarttoggle": {
+            var objects = document.getElementsByClassName("SanBart");
+            for (var i = 0; i < objects.length; i++) {
+                objects[i].classList.toggle("on")
+            }
+        };
+        break;
+        case "elMiratoggle": {
+            var objects = document.getElementsByClassName("elMira");
+            for (var i = 0; i < objects.length; i++) {
+                objects[i].classList.toggle("on")
+            }
+        };
+        break;
+        case "tikaltoggle": {
+            var objects = document.getElementsByClassName("tikal");
+            for (var i = 0; i < objects.length; i++) {
+                objects[i].classList.toggle("on")
+            }
+        };
+        break;
+        case "copantoggle": {
+            var objects = document.getElementsByClassName("copan");
+            for (var i = 0; i < objects.length; i++) {
+                objects[i].classList.toggle("on")
+            }
+        };
+        break;
+        case "chItzatoggle": {
+            var objects = document.getElementsByClassName("chItza");
+            for (var i = 0; i < objects.length; i++) {
+                objects[i].classList.toggle("on")
+            }
+        };
+        break;
+        case "izapatoggle": {
+            var objects = document.getElementsByClassName("izapa");
+            for (var i = 0; i < objects.length; i++) {
+                objects[i].classList.toggle("on")
+            }
+        };
+        break;
+        case "KJtoggle": {
+            var objects = document.getElementsByClassName("KJ");
+            for (var i = 0; i < objects.length; i++) {
+                objects[i].classList.toggle("on")
+            }
+        };
+        break;
+        case "palenQtoggle": {
+            var objects = document.getElementsByClassName("palenQ");
+            for (var i = 0; i < objects.length; i++) {
+                objects[i].classList.toggle("on")
+            }
+        };
+        break;
+        case "uxmaltoggle": {
+            var objects = document.getElementsByClassName("uxmal");
+            for (var i = 0; i < objects.length; i++) {
+                objects[i].classList.toggle("on")
+            }
+        };
+        break;
+        case "yaxchiltoggle": {
+            var objects = document.getElementsByClassName("yaxchil");
             for (var i = 0; i < objects.length; i++) {
                 objects[i].classList.toggle("on")
             }
